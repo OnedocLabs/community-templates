ed1d727bea38529afa893a042343a917d20b362f
mainak <heellomeelo@gmail.com>
2024-02-29 09:43:20 +0530

    cv added

---
+++ templates/CV/render.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Onedoc } from "@onedoc/client";
+import fs from "fs";
+import {compile} from "@onedoc/react-print"
+import {CV} from "./src/main.tsx" 
+
+const onedoc = new Onedoc(process.env.ONEDOC_API_KEY!);
+
+(async () => {
+
+  const html = await compile(<CV/>);
+  
+
+  const { file, error } = await onedoc.render({
+    html,
+    test:false,
+    save:false,
+    assets: [
+      {
+        path: "./util/util.css",
+        content: readFileSync(join(process.cwd(), "./util/util.css")).toString(),
+      },
+    ],
+  });
+
+  console.log(error)
+  
+  fs.writeFileSync("./cv.pdf", Buffer.from(file));
+
+})();
